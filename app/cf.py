88970e77f5504dd96b24c8a6d8e3b66b389a41da
sovanny <sovhu774@student.liu.se>
2017-11-28 20:11:43 +0900

    for heroku

--- app/cf.py
+++ app/cf.py
@@ -62,68 +62,68 @@ def loadMockData(steamid):
 
 
 
-df = pd.read_csv('./app/static/data/steam-200k.csv')
+# df = pd.read_csv('./app/static/data/steam-200k.csv')
 
-#select played games
-edgelist = [[Userid,gameTitle,10*(1+playTime**(-2))**(-4.0)] for Userid,gameTitle,p,playTime,e in df.values if p =="play"]
+# #select played games
+# edgelist = [[Userid,gameTitle,10*(1+playTime**(-2))**(-4.0)] for Userid,gameTitle,p,playTime,e in df.values if p =="play"]
 
-gamerList = []
-gameList = []
-for i in edgelist:
-    gamerList.append(i[0])
-    gameList.append(i[1])
-AllgameList = set(gameList)
-gamerList = set(gamerList)
-#print '# of gamer in csv is ', len(gamerList)
-#print '# of game in csv is ',len(AllgameList)
+# gamerList = []
+# gameList = []
+# for i in edgelist:
+#     gamerList.append(i[0])
+#     gameList.append(i[1])
+# AllgameList = set(gameList)
+# gamerList = set(gamerList)
+# #print '# of gamer in csv is ', len(gamerList)
+# #print '# of game in csv is ',len(AllgameList)
 
-G = nx.Graph()
-G.add_weighted_edges_from(edgelist)
-df = nx.to_pandas_dataframe(G)
-GG_matrix = DataFrame(df, columns = AllgameList, index = gamerList)
+# G = nx.Graph()
+# G.add_weighted_edges_from(edgelist)
+# df = nx.to_pandas_dataframe(G)
+# GG_matrix = DataFrame(df, columns = AllgameList, index = gamerList)
 
 
-def mainCF(steamid): 
-    similaruser = find_10similarUsers(steamid)
-    return expectedScoreToGame(similaruser)
+# def mainCF(steamid): 
+#     similaruser = find_10similarUsers(steamid)
+#     return expectedScoreToGame(similaruser)
     
-def expectedScoreToGame(similarUsers):
-    a = []
-    for i,j in similarUsers.items():
-        b = {}
-        for k,l in dict(GG_matrix.ix[i]).items():
-            if l*j != 0:
-                b[k] = l*j
-        a.append(b)
+# def expectedScoreToGame(similarUsers):
+#     a = []
+#     for i,j in similarUsers.items():
+#         b = {}
+#         for k,l in dict(GG_matrix.ix[i]).items():
+#             if l*j != 0:
+#                 b[k] = l*j
+#         a.append(b)
         
-    from collections import Counter
-    expectedScore = Counter()
-    for i in a:
-        x = Counter(i)
-        expectedScore.update(x)
-    expectedScoreForSorting = dict(expectedScore).items()  
-    expectedScoreForSorting.sort( key=lambda x: x[1] , reverse=True )
+#     from collections import Counter
+#     expectedScore = Counter()
+#     for i in a:
+#         x = Counter(i)
+#         expectedScore.update(x)
+#     expectedScoreForSorting = dict(expectedScore).items()  
+#     expectedScoreForSorting.sort( key=lambda x: x[1] , reverse=True )
 
-    return expectedScoreForSorting
+#     return expectedScoreForSorting
         
-def find_10similarUsers(a):
-    Usersim = {}
-    for i in gamerList:
-        usercosim = cos_sim(list(GG_matrix.ix[a]),list(GG_matrix.ix[i]))
-        if usercosim != 0 and i!=a :
-            Usersim[i] = usercosim
+# def find_10similarUsers(a):
+#     Usersim = {}
+#     for i in gamerList:
+#         usercosim = cos_sim(list(GG_matrix.ix[a]),list(GG_matrix.ix[i]))
+#         if usercosim != 0 and i!=a :
+#             Usersim[i] = usercosim
     
-    Usersimd = Usersim.items()
-    Usersimd.sort( key=lambda x: x[1] , reverse=True )
-    find_10similarUsers = {}
-    for i in range(0,20):
-        find_10similarUsers[Usersimd[i][0]] = Usersimd[i][1]  
-    return find_10similarUsers 
+#     Usersimd = Usersim.items()
+#     Usersimd.sort( key=lambda x: x[1] , reverse=True )
+#     find_10similarUsers = {}
+#     for i in range(0,20):
+#         find_10similarUsers[Usersimd[i][0]] = Usersimd[i][1]  
+#     return find_10similarUsers 
           
-def cos_sim(a, b):
-    from numpy import dot
-    from numpy.linalg import norm
-    cos_sim = dot(a, b)/(norm(a)*norm(b))
-    return cos_sim
+# def cos_sim(a, b):
+#     from numpy import dot
+#     from numpy.linalg import norm
+#     cos_sim = dot(a, b)/(norm(a)*norm(b))
+#     return cos_sim
 
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session
 from app import app
 from .forms import LoginForm
-from .cf import loadMockData, mainCF
+from .cf import loadMockData #, mainCF
 from .dataformatter import tupleToDict
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,9 +20,9 @@ def login():
 def feed():
     steamid = session.get('session_steamid', None)
     #user = {'nickname': 'Miguel'}  # fake user
-    #games = loadMockData(steamid)
-    games = mainCF(151603712)[:10]
-    games = tupleToDict(games)
+    games = loadMockData(steamid)
+    #games = mainCF(151603712)[:10]
+    #games = tupleToDict(games)
     return render_template("feed.html",
                            steamid=steamid,
                            games=games)
