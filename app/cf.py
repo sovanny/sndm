054881897408a845dfb98480746fc201159b859c
sovanny <sovhu774@student.liu.se>
2017-12-05 23:12:08 +0900

    psoidfo

--- app/cf.py
+++ app/cf.py
@@ -9,74 +9,13 @@ from .igdb_functions import getGameInfo
 def loadMockData(steamid):
     mockdata = []
 
-    game = getGameInfo('The Elder Scrolls V Skyrim')
-    
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore': 54,
-        'igdbUrl': game['url']
-    })
-
-    game = getGameInfo('Fallout New Vegas')
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore':  36,
-        'igdbUrl': game['url']
-    })
-
-    game = getGameInfo('Left 4 Dead 2')
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore': 33,
-        'igdbUrl': game['url']
-    })
-
-    game = getGameInfo('Team Fortress 2')
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore': 29,
-        'igdbUrl': game['url']
-    })
-
-    game = getGameInfo('Fallout 4')
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore': 16,
-        'igdbUrl': game['url']
-    })
-
-    game = getGameInfo('Spore')
-    mockdata.append({
-        'gameTitle': game['title'],        
-        'summary': game['summary'],
-        'rating': game['rating'],
-        'cover': game['cover'],
-        'similarityScore': 8,
-        'igdbUrl': game['url']
-    })
-    
     game = getGameInfo('POSTAL 2')
     mockdata.append({
         'gameTitle': game['title'],        
         'summary': game['summary'],
         'rating': game['rating'],
         'cover': game['cover'],
-        'similarityScore': 7,
+        'similarityScore': 100,
         'igdbUrl': game['url']
     })
 
@@ -86,7 +25,7 @@ def loadMockData(steamid):
         'summary': game['summary'],
         'rating': game['rating'],
         'cover': game['cover'],
-        'similarityScore': 7,
+        'similarityScore':  95,
         'igdbUrl': game['url']
     })
 
@@ -96,20 +35,183 @@ def loadMockData(steamid):
         'summary': game['summary'],
         'rating': game['rating'],
         'cover': game['cover'],
-        'similarityScore': 5,
+        'similarityScore': 67,
         'igdbUrl': game['url']
     })
 
-    game = getGameInfo('Tomb Raider')
+    game = getGameInfo('The Forest')
     mockdata.append({
         'gameTitle': game['title'],        
         'summary': game['summary'],
         'rating': game['rating'],
         'cover': game['cover'],
-        'similarityScore': 4,
+        'similarityScore': 62,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Hatoful Boyfriend')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 60,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Bastion')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 57,
         'igdbUrl': game['url']
     })
     
+    game = getGameInfo("Sid Meier's Civilization V")
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 53,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Counter-Strike Condition Zero')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 53,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Dead Island')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 52,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Counter-Strike Global Offensive')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 51,
+        'igdbUrl': game['url']
+    })
+    
+    return mockdata
+
+def loadMockData2(games):
+    mockdata = []
+
+    game = getGameInfo('Bulletstorm')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 98,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('DC Universe Online')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 90,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Aliens vs. Predator')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 67,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Monopoly')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 65,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Mortal Kombat X')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 147,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Far Cry 2')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 61,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Dying Light')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 57,
+        'igdbUrl': game['url']
+    })
+
+    game = getGameInfo('Strider')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 56,
+        'igdbUrl': game['url']
+    })
+    game = getGameInfo('The Flame in the Flood')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 55,
+        'igdbUrl': game['url']
+    })
+    game = getGameInfo('Scribblenauts Unlimited')
+    mockdata.append({
+        'gameTitle': game['title'],        
+        'summary': game['summary'],
+        'rating': game['rating'],
+        'cover': game['cover'],
+        'similarityScore': 51,
+        'igdbUrl': game['url']
+    })
+
     return mockdata
 
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session
 from app import app
 from .forms import LoginForm, ThreeGamesForm
-from .cf import loadMockData #, mainCF
+from .cf import loadMockData, loadMockData2 #, mainCF
 from .dataformatter import tupleToDict
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,37 +12,52 @@ def login():
     if loginForm.validate_on_submit():
         session['session_steamid'] = loginForm.steamid.data
         #flash('Login requested for SteamID="%s"' % loginForm.steamid.data)
-        session['game1'] = "DARK SOULS III"
-        session['game2'] = "Mount & Blade: Warband"
-        session['game3'] = "Sid Meier's Civilization V"
+        # these games should be fetched from steam api
+        session['game1'] = "The Elder Scrolls V Skyrim"
+        session['game2'] = "Fallout 4"
+        session['game3'] = "Spore"
+        session['loginType'] = 1
         return redirect('/feed')
     if threeGamesForm.validate_on_submit():
         session['session_steamid'] = threeGamesForm.username.data
         session['game1'] = threeGamesForm.game1.data
         session['game2'] = threeGamesForm.game2.data
         session['game3'] = threeGamesForm.game3.data
+        session['loginType'] = 2       
         return redirect('/feed')      
         
     return render_template('login.html', 
-                           #title='Sign In',
                            loginForm=loginForm,
                            threeGamesForm = threeGamesForm)
 
 @app.route('/feed')
 def feed():
     steamid = session.get('session_steamid', None)
-    #user = {'nickname': 'Miguel'}  # fake user
-    games = loadMockData(steamid)
-    #games = mainCF(151603712)[:10]
-    #games = tupleToDict(games)
 
-    game1 = session.get('game1', None)
-    game2 = session.get('game2', None)
-    game3 = session.get('game3', None)
-    playedGames = [game1, game2, game3]    
+    if session.get('loginType', None) == 1:
+        #games = mainCF(151603712)[:10]
+        #games = tupleToDict(games)
+        # should not be loadMockData in real app
+        games = loadMockData(steamid)
+        game1 = session.get('game1', None)
+        game2 = session.get('game2', None)
+        game3 = session.get('game3', None)
+        playedGames = [game1, game2, game3]    
+        profilePic = 'ron-winkle.jpg'
+        steamid = 'Ron Winkle'
+    elif session.get('loginType', None) == 2:
+        game1 = session.get('game1', None)
+        game2 = session.get('game2', None)
+        game3 = session.get('game3', None)
+        playedGames = [game1, game2, game3]    
+        # should not be loadMockData in real app
+        games = loadMockData2(playedGames)
+        profilePic = 'wonder-woman.png'
+
             
     return render_template("feed.html",
                            steamid=steamid,
                            games=games,
-                           playedGames = playedGames)
+                           playedGames = playedGames,
+                           profilePic = profilePic)
 
